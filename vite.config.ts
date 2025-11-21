831024d5cc2ea0dba6692249728cd1217ee9e774
Xavier Shay <contact@xaviershay.com>
2025-11-22 08:38:11 +1100

    can load on phone

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Listen on all network interfaces
+    allowedHosts: ['apollo.local'],
+  },
 })
